29e4bb7f6e7bd3e07233e38a020a15ac2161a93b
Emmanuel Ikhide-Solomon <emmanuelikhide07@gmail.com>
2025-09-19 23:27:47 +0100

    My Personal Solution to Ex2

---
+++ part1_data_structures/exercises/ex2_pixelgrid.py
@@ -0,0 +1,29 @@
+"""
+Create a class `PixelGrid` that:
+- Represents a 2D image grid of pixels (width x height).
+- Supports `__getitem__` and `__setitem__` for accessing/modifying pixels.
+- Supports slicing rows or columns.
+- Provides `__len__` to return the number of rows.
+- Implements `__repr__` to display a small preview (e.g., 5x5 section).
+
+
+Example:
+
+>>> pg = PixelGrid(10, 10)
+>>> pg[0, 0] = (255, 0, 0) # Set top-left pixel red
+>>> print(pg[0, 0]) # (255, 0, 0)
+>>> print(len(pg)) # 10
+
+"""
+
+class PixelGrid():
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        
+
+    def __repr__(self):
+        pass
+
+    def __getitem__(self):
+        pass
